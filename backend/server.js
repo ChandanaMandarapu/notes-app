fbb5f20aa5e9707a78347a4629b5bf9acf9c3211
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2025-05-21 06:41:13 +0530

    api's

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
-// import express from "express"
+import express from "express"
 
-const express = require("express") 
+// const express = require("express")
+// first api  
 
 const app = express()
+
+app.get("/api/notes",(req,res) => {
+    res.send("you got 5notes")
+})
 app.listen(5001,() => {
     console.log("server started on port")
 })
